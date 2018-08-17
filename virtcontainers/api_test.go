afa3861530e72d78d70da35f0c89f9d3f1882236
Sebastien Boeuf <sebastien.boeuf@intel.com>
2018-08-16 21:57:04 -0700

    hook: Move OCI hooks handling to the CLI
    The CLI being the implementation of the OCI specification, and the
    hooks being OCI specific, it makes sense to move the handling of any
    OCI hooks to the CLI level. This changes allows the Kata API to
    become OCI agnostic.
    
    Fixes #599
    
    Signed-off-by: Sebastien Boeuf <sebastien.boeuf@intel.com>

--- virtcontainers/api_test.go
+++ virtcontainers/api_test.go
@@ -141,24 +141,12 @@ func newTestSandboxConfigHyperstartAgentDefaultNetwork() SandboxConfig {
 		SockTtyName: testHyperstartTtySocket,
 	}
 
-	hooks := Hooks{
-		PreStartHooks: []Hook{
-			{
-				Path: getMockHookBinPath(),
-				Args: []string{testKeyHook, testContainerIDHook, testControllerIDHook},
-			},
-		},
-		PostStartHooks: []Hook{},
-		PostStopHooks:  []Hook{},
-	}
-
 	netConfig := NetworkConfig{
-		NumInterfaces: len(hooks.PreStartHooks),
+		NumInterfaces: 1,
 	}
 
 	sandboxConfig := SandboxConfig{
-		ID:    testSandboxID,
-		Hooks: hooks,
+		ID: testSandboxID,
 
 		HypervisorType:   MockHypervisor,
 		HypervisorConfig: hypervisorConfig,
