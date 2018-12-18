72a28906f9efca919a5a9ba056ad7ee8d336cf61
Manohar Castelino <manohar.r.castelino@intel.com>
2018-12-18 12:53:19 -0800

    firecracker: Setup firecracker capabilities
    - 9p is unsupported by firecracker
    - Enable pseudo hotplug block device hotplug capability
    
    Initially, this will be a pseudo capability for Firecracker hypervisor,
    but we will utilize a pool of block devices and block device rescan as a
    temporary workaround.
    
    Signed-off-by: Manohar Castelino <manohar.r.castelino@intel.com>

--- virtcontainers/fc.go
+++ virtcontainers/fc.go
@@ -605,8 +605,8 @@ func (fc *firecracker) capabilities() capabilities {
 	span, _ := fc.trace("capabilities")
 	defer span.Finish()
 	var caps capabilities
-	//caps.set9pUnsupported()
-	//caps.setHotplugUnsupported()
+	caps.set9pUnsupported()
+	caps.setBlockDeviceHotplugSupport()
 
 	return caps
 }
